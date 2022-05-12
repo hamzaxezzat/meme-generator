2acf70b7546f98602d87ffce695997c310941df4
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-12 17:12:30 +0200

    First

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Form() {
+    return (
+        <form>
+            <div className="input">
+                <input></input>
+                <input></input>
+            </div>
+            <input className="submit">Submit</input>
+        </form>
+    );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <nav>
+            <div className="header">
+                <img src="./Troll-Face.png"></img>
+                <h3>Meme Generator</h3>
+                <h5>React Course - Project 3</h5>
+            </div>
+        </nav>
+    );
+}
